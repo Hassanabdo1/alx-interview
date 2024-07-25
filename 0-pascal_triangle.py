73449659739ac041b0c2e2771b3f9a8116172fe8
Hassanabdo1 <Hassanabdelftah@outlook.com>
2024-07-26 00:28:23 +0300

    Promis

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,31 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
+
+# 0-main.py
+if __name__ == "__main__":
+    """
+    0-main
+    """
+    pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
+
+    def print_triangle(triangle):
+        """
+        Print the triangle
+        """
+        for row in triangle:
+            print("[{}]".format(",".join([str(x) for x in row])))
+
+    print_triangle(pascal_triangle(5))
+
