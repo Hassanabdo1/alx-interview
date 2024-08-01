5dc4b2ca214571acce09312c2fa75c029c29f9bc
Hassanabdo1 <Hassanabdelftah@outlook.com>
2024-08-02 01:53:32 +0300

    InterView

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,25 @@
+def canUnlockAll(boxes):
+    n = len(boxes)
+    opened = [False] * n
+    opened[0] = True  # The first box is initially unlocked
+    keys = [0]  # Start with the key to the first box
+
+    while keys:
+        current_key = keys.pop()
+        for key in boxes[current_key]:
+            if key < n and not opened[key]:
+                opened[key] = True
+                keys.append(key)
+
+    return all(opened)
+
+# Test cases
+boxes = [[1], [2], [3], [4], []]
+print(canUnlockAll(boxes))  # True
+
+boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+print(canUnlockAll(boxes))  # True
+
+boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+print(canUnlockAll(boxes))  # False
+
